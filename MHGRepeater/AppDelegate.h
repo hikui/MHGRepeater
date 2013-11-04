0491479031747e28f691a8cadb753ffe1b43fcdd
hikui <hikuimiao@gmail.com>
2013-11-04 14:19:00 +0800

    Initial Commit

---
+++ MHGRepeater/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MHGRepeater
+//
+//  Created by 缪 和光 on 13-11-4.
+//  Copyright (c) 2013年 Hokuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MHGRepeaterTests/MHGRepeaterTests.m
@@ -0,0 +1,34 @@
+//
+//  MHGRepeaterTests.m
+//  MHGRepeaterTests
+//
+//  Created by 缪 和光 on 13-11-4.
+//  Copyright (c) 2013年 Hokuang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MHGRepeaterTests : XCTestCase
+
+@end
+
+@implementation MHGRepeaterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
